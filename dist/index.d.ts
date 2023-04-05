76c8bbcf7e96965ec7f59d4483db39266e464002
Marc Güell Segarra <marc@ondori.dev>
2023-04-05 09:23:14 +0200

    Add dist

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+import Purchases from './purchases';
+export default Purchases;
+export * from './errors';
+export * from './purchaserInfo';
+export * from './purchases';
+export * from './offerings';
